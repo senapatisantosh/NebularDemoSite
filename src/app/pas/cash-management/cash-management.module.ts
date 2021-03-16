007135567cb188d9993101a603d14657f5368dd8
SANTOSH KUMAR SENAPATI <45514742+senapatisantosh@users.noreply.github.com>
2021-03-16 10:02:18 +0530

    Jvector Map Integration

--- src/app/pas/cash-management/cash-management.module.ts
+++ src/app/pas/cash-management/cash-management.module.ts
@@ -5,10 +5,14 @@ import { DepositComponent } from './deposit/deposit.component';
 import { ReconciliationComponent } from './reconciliation/reconciliation.component';
 import { ReportComponent } from './report/report.component';
 import { CashManagementComponent } from './cash-management.component';
+import { NbCardModule } from '@nebular/theme';
+import { ThemeModule } from '../../@theme/theme.module';
+import { VectormapDirective } from './cash-up/vectormap.directive';
 
 
 @NgModule({
-  imports: [CashManagementRoutingModule],
-  declarations: [CashManagementComponent, CashUpComponent, DepositComponent, ReconciliationComponent, ReportComponent],
+  imports: [CashManagementRoutingModule, ThemeModule, NbCardModule],
+  declarations: [CashManagementComponent, CashUpComponent, DepositComponent, ReconciliationComponent, ReportComponent, VectormapDirective],
+  exports: [VectormapDirective],
 })
 export class CashManagementModule { }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import './vendor.ts';
 
 if (environment.production) {
   enableProdMode();
